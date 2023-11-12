cb464741e4b3285b1d97565afcac550ff8822bb9
ospodaryk <ospodaryk11@gmail.com>
2023-11-12 15:24:42 +0200

    Added method

--- src/main/java/com/project/appz/repository/MedicalRecordRepository.java
+++ src/main/java/com/project/appz/repository/MedicalRecordRepository.java
@@ -1,9 +1,16 @@
 package com.project.appz.repository;
 
 import com.project.appz.entities.MedicalRecord;
+import com.project.appz.enums.Disease;
+import com.project.appz.entities.User;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MedicalRecordRepository extends JpaRepository<MedicalRecord, Long> {
+    List<MedicalRecord> findByUser(User user);
+    List<MedicalRecord> findByUserAndDisease(User user, Disease disease);
 }
