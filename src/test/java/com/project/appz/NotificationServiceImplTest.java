43f2e4c8ede7b16fc5d76d46963b6bd4c929c24f
Artem Huk <artem.huk.si@gmail.com>
2023-11-25 12:07:22 +0200

    Revert "Add NotificationServiceImplTest"
    This reverts commit 5e350779cf9c1cc364f6fc0672f184724a1010c2.

--- src/test/java/com/project/appz/NotificationServiceImplTest.java
+++ src/test/java/com/project/appz/NotificationServiceImplTest.java
@@ -13,7 +13,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(MockitoExtension.class)
 public class NotificationServiceImplTest {
@@ -29,12 +28,18 @@ public class NotificationServiceImplTest {
     // Additional Positive Tests
     @Test
     public void testSendNotificationWithValidNotification() {
-        assertTrue(true);
+        Notification notification = new Notification();
+        notification.setMessage("Valid Message");
+        notificationService.sendNotification(notification);
+        Mockito.verify(logger).log("Sending notification: Valid Message");
     }
 
     @Test
     public void testScheduleNotificationWithValidInputs() {
-        assertTrue(true);
+        Notification notification = new Notification();
+        Date scheduledTime = new Date();
+        notificationService.scheduleNotification(notification, scheduledTime);
+        Mockito.verify(logger).log("Scheduling notification for: " + scheduledTime.toString());
     }
 
     // Negative Tests
@@ -42,21 +47,34 @@ public class NotificationServiceImplTest {
     public void testSendNotificationWithNullNotification() {
         // Depending on implementation, expect an exception or specific behavior
         // Example: Expect an IllegalArgumentException
-        assertTrue(true);
+        assertThrows(IllegalArgumentException.class, () -> {
+            notificationService.sendNotification(null);
+        });
     }
 
     @Test
     public void testScheduleNotificationWithNullInputs() {
-        assertTrue(true);
+        assertThrows(IllegalArgumentException.class, () -> {
+            notificationService.scheduleNotification(null, new Date());
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            notificationService.scheduleNotification(new Notification(), null);
+        });
     }
 
     @Test
     public void testSendNotificationWithEmptyMessage() {
-        assertTrue(true);
+        Notification emptyMessageNotification = new Notification();
+        emptyMessageNotification.setMessage("");
+        notificationService.sendNotification(emptyMessageNotification);
+        // Verify based on expected behavior
     }
 
     @Test
     public void testScheduleNotificationWithPastDate() {
-        assertTrue(true);
+        Notification notification = new Notification();
+        Date pastDate = new Date(System.currentTimeMillis() - 100000); // Past date
+        notificationService.scheduleNotification(notification, pastDate);
+        // Verify based on expected behavior
     }
 }
