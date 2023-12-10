a43c104bc1df001bf339d4f82dd70a3035cda0cc
ospodaryk <ospodaryk11@gmail.com>
2023-12-10 21:41:39 +0200

    Fixed

--- src/main/java/com/project/appz/repository/PollAssignmentRepository.java
+++ src/main/java/com/project/appz/repository/PollAssignmentRepository.java
@@ -11,17 +11,17 @@ import java.util.List;
 @Repository
 public interface PollAssignmentRepository extends JpaRepository<PollAssignment, Long> {
     @Query(value = "SELECT id, created_date, deadline, is_completed, doctor_id, poll_id, user_id\n" +
-            "FROM public.assigned_poll\n" +
+            "FROM public.poll_assignment\n" +
             "WHERE user_id = :userId AND is_completed = true", nativeQuery = true)
     public List<PollAssignment> findByUserIdAndIsCompleted(@Param("userId") Long userId);
 
     @Query(value = "SELECT id, created_date, deadline, is_completed, doctor_id, poll_id, user_id " +
-            "FROM public.assigned_poll " +
+            "FROM public.poll_assignment " +
             "WHERE user_id = :userId AND poll_id = :pollId", nativeQuery = true)
     public PollAssignment findByPollIdAndUserId(@Param("userId") Long userId, @Param("pollId") Long pollId);
 
     @Query(value = "SELECT id, created_date, deadline, is_completed, doctor_id, poll_id, user_id\n" +
-            "FROM public.assigned_poll\n" +
+            "FROM public.poll_assignment\n" +
             "WHERE user_id = :userId AND is_completed = false", nativeQuery = true)
     public List<PollAssignment> findByUserId(@Param("userId") Long user);
 
