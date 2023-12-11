c77fb1c06fe4a5d13720ac20868712679c397120
ospodaryk <ospodaryk11@gmail.com>
2023-12-11 12:03:41 +0200

    Fixed

--- src/main/java/com/project/appz/service/notification/impl/EmailServiceImpl.java
+++ src/main/java/com/project/appz/service/notification/impl/EmailServiceImpl.java
@@ -1,24 +1,23 @@
 package com.project.appz.service.notification.impl;
 
 import com.project.appz.service.notification.EmailService;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailServiceImpl implements EmailService {
-    @Override
-    public void sendMessage(String to, String subject, String text) {
 
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendMessage(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("appz.rehabilitation.service@gmail.com");
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        mailSender.send(message);
     }
-//
-//    @Autowired
-//    private JavaMailSender mailSender;
-//
-//    public void sendMessage(String to, String subject, String text) {
-//        SimpleMailMessage message = new SimpleMailMessage();
-//        message.setFrom("appz.rehabilitation.service@gmail.com");
-//        message.setTo(to);
-//        message.setSubject(subject);
-//        message.setText(text);
-//        mailSender.send(message);
-//    }
 }
